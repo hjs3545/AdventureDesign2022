60ff4a1abd7fd974b38ff98cdb41b5c438afccf9
hjs3545 <hjs3545@gmail.com>
2022-11-20 21:14:22 +0900

    2번째 설명화면 까지 구현

--- FOODCHEAP/app/src/main/java/com/example/FOODCHEAP/MainActivity.java
+++ FOODCHEAP/app/src/main/java/com/example/FOODCHEAP/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button button1 = (Button) findViewById(R.id.button1);
 
+
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+
+
+
+
+
+
+
     }
 }
 
--- FOODCHEAP/app/src/main/java/com/example/FOODCHEAP/explain1.java
+++ FOODCHEAP/app/src/main/java/com/example/FOODCHEAP/explain1.java
@@ -1,6 +1,10 @@
 package com.example.FOODCHEAP;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +15,14 @@ public class explain1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.explain1);
 
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), explain2.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
