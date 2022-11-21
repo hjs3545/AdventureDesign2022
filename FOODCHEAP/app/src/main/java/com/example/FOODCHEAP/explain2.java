10579139cb49e0bbd143b044d29d94b157681ae3
hjs3545 <hjs3545@gmail.com>
2022-11-21 12:56:47 +0900

    3번쨰 설면화면까지 구현

--- FOODCHEAP/app/src/main/java/com/example/FOODCHEAP/explain2.java
+++ FOODCHEAP/app/src/main/java/com/example/FOODCHEAP/explain2.java
@@ -1,6 +1,9 @@
 package com.example.FOODCHEAP;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,14 @@ public class explain2 extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.explain2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), explain3.class);
+                startActivity(intent);
+            }
 
+        });
     }
 }
